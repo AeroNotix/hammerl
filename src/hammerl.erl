89443c26784f51491d3bd2f26302addd30e04c26
Aaron <aaron.l.france@gmail.com>
2013-06-15 20:10:05 +0200

    Added the blog-related functions.

--- src/hammerl.erl
+++ src/hammerl.erl
@@ -1,7 +1,7 @@
 -module(hammerl).
 
 %% API.
--export([start/0, dispatchers/0, reload_dispatchers/0]).
+-export([start/0, dispatchers/0, reload_dispatchers/0, blog/1]).
 
 %% API.
 
@@ -9,13 +9,26 @@ start() ->
     ok = application:start(hammerl).
 
 dispatchers() ->
-    cowboy_router:compile([
-        {'_', [
-            {"/blog/[:blogname]", blog, []},
-            {"/", index, []}
-        ]}
-    ]).
+	emysql:add_pool(blog_pool,
+					1,
+					connection:username(),
+					connection:password(),
+					connection:host(),
+					connection:port(),
+					"blog",
+					utf8),
+	emysql:prepare(blog_stmt_get,
+				   <<"SELECT * from blog_entry WHERE blog_title=?">>),
+	cowboy_router:compile([
+		{'_', [
+			{"/blog/[:blogname]", blog, []},
+			{"/", index, []}
+		]}
+	]).
 
 reload_dispatchers() ->
-    cowboy:set_env(http, dispatch,
-                   dispatchers()).
+	cowboy:set_env(http, dispatch,
+				   dispatchers()).
+
+blog(Name) ->
+	emysql:execute(blog_pool, blog_stmt_get, [Name]).
