3cbf1fb60e9e91d51e6a69805baaa78ef8a231d9
Aaron <aaron.l.france@gmail.com>
2013-06-15 22:22:43 +0200

    Added the handler part so it will correctly retrieve a blog entry when it's name is hit on the url.

--- src/blog.erl
+++ src/blog.erl
@@ -1,10 +1,24 @@
 -module(blog).
 
--export([init/3, handle/2]).
+-export([init/3, handle/2, terminate/3]).
+
+-include("blog.hrl").
 
 init(_Transport, Req, []) ->
     {ok, Req, undefined}.
 
 handle(Req, State) ->
-    cowboy_req:reply(200, [], <<"OK">>, Req),
+	{BlogName, Req2} = cowboy_req:binding(blogname, Req),
+	case BlogName of
+		undefined ->
+			cowboy_req:reply(404, [], do404(), Req2);
+		_Else ->
+			Blog = hammerl:blog(binary_to_list(BlogName)),
+			case Blog of
+				{error, not_found} ->
+					cowboy_req:reply(404, [], do404(), Req2);
+				_Else2 ->
+					cowboy_req:reply(200, [], Blog#blog.content, Req2)
+			end
+	end,
     {ok, Req, State}.
