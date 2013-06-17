612119416239281772358cadac71189a5f09f290
AeroNotix <aaron.l.france@gmail.com>
2013-06-17 09:21:02 +0100

    Fix up tabs in hammerl.

--- src/hammerl.erl
+++ src/hammerl.erl
@@ -7,46 +7,49 @@
 
 %% API.
 start() ->
-	ok = application:start(hammerl).
+    ok = application:start(hammerl).
 
 dispatchers() ->
-	cowboy_router:compile([
-		{'_', [
-			{"/blog/[:blogname]", blog, []},
-            {"/static/[...]", cowboy_static, [
-                {directory, {priv_dir, hammerl, [<<"static">>]}},
-                {mimetypes, {fun mimetypes:path_to_mimes/2, default}}
-            ]},
-			{"/about", about, []},
-			{"/", index, []}
-		]}
-	]).
+    cowboy_router:compile([
+                           {'_', [
+                                  {"/blog/[:blogname]", blog, []},
+                                  {"/static/[...]", cowboy_static,
+                                   [
+                                    {directory, {priv_dir, hammerl, [<<"static">>]}},
+                                    {mimetypes, {fun mimetypes:path_to_mimes/2, default}}
+                                   ]
+                                  },
+                                  {"/about", about, []},
+                                  {"/", index, []}
+                                 ]
+                           }
+                          ]).
 
 reload_dispatchers() ->
-	cowboy:set_env(http, dispatch,
-				   dispatchers()).
+    cowboy:set_env(http, dispatch,
+                   dispatchers()).
 
 %% @doc
 %% blog/1 will return the associated blog entry for the URL that you
 %% provide.
 %% @spec blog(Name::string()) -> blog() | {error, not_found}
 blog(Name) ->
-	{_, _, _, Blog, _} = emysql:execute(blog_pool, blog_stmt_get, [Name]),
-	case length(Blog) of
-		0 ->
-			{error, not_found};
-		_N ->
-			{ID, Date, URL, Title, Content} = list_to_tuple(lists:nth(1, Blog)),
-			#blog{id = ID, date = Date, url = URL, title = Title, content = Content}
-	end.
+    {_, _, _, Blog, _} = emysql:execute(blog_pool, blog_stmt_get, [Name]),
+    case length(Blog) of
+        0 ->
+            {error, not_found};
+        _N ->
+            {ID, Date, URL, Title, Content} = list_to_tuple(lists:nth(1, Blog)),
+            #blog{id = ID, date = Date, url = URL, title = Title, content = Content}
+    end.
 
 %% @doc
 %% bloglist/0 returns a proplist containing all the blogs in the
 %% database.
 %% @spec bloglist() -> list()
 bloglist() ->
-	{_, _, _, Blogs, _} = emysql:execute(blog_pool, <<"SELECT * FROM blog_entry">>),
-	create_blog_list(Blogs).
+    {_, _, _, Blogs, _} = emysql:execute(blog_pool, <<"SELECT * FROM blog_entry">>),
+    create_blog_list(Blogs).
 
 %% @doc
 %% create_blog_list/1 takes a list of blog entries taken from the
@@ -54,11 +57,11 @@ bloglist() ->
 %% interpolated into a template easily.
 %% @spec create_blog_list(L::list()) -> list()
 create_blog_list([]) ->
-	[];
+    [];
 create_blog_list(L) ->
-	create_blog_list(L, []).
+    create_blog_list(L, []).
 create_blog_list([], BlogEntries) ->
-	BlogEntries;
+    BlogEntries;
 create_blog_list([H|T], BlogEntries) ->
-	{ID, Date, URL, Title, Content} = list_to_tuple(H),
-	create_blog_list(T, [#blog{id = ID, date = Date, url = URL, title = Title, content = Content}|BlogEntries]).
+    {ID, Date, URL, Title, Content} = list_to_tuple(H),
+    create_blog_list(T, [#blog{id = ID, date = Date, url = URL, title = Title, content = Content}|BlogEntries]).
