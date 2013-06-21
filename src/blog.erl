13caae3bd79c5c91142f77986dade2cc4f5db037
AeroNotix <aaron.l.france@gmail.com>
2013-06-21 11:59:57 +0100

    Tab fix.

--- src/blog.erl
+++ src/blog.erl
@@ -10,22 +10,22 @@ init(_Transport, Req, []) ->
 handle(Req, State) ->
     {BlogName, Req2} = cowboy_req:binding(blogname, Req),
     case BlogName of
-	undefined ->
-	    Blogs = [[{blog_title, Blog#blog.title},
-		      {blog_url, Blog#blog.url},
-		      {date, context:strdate(Blog#blog.date)}] || Blog <- hammerl:bloglist()],
-	    cowboy_req:reply(200, [], templates:blogmode([{blog, Blogs}] ++ context:ctx()), Req2);
-	_Else ->
-	    Blog = hammerl:blog(binary_to_list(BlogName)),
-	    case Blog of
-		{error, not_found} ->
-		    cowboy_req:reply(404, [], gen_handlers:do404(), Req2);
-		_Else2 ->
-		    cowboy_req:reply(200, [], templates:blog([
-							      {blog, [{blog_title, Blog#blog.title},
-								      {blog_post, Blog#blog.content}]}
-							     ] ++ context:ctx()), Req2)
-	    end
+        undefined ->
+            Blogs = [[{blog_title, Blog#blog.title},
+                      {blog_url, Blog#blog.url},
+                      {date, context:strdate(Blog#blog.date)}] || Blog <- hammerl:bloglist()],
+            cowboy_req:reply(200, [], templates:blogmode([{blog, Blogs}] ++ context:ctx()), Req2);
+        _Else ->
+            Blog = hammerl:blog(binary_to_list(BlogName)),
+            case Blog of
+                {error, not_found} ->
+                    cowboy_req:reply(404, [], gen_handlers:do404(), Req2);
+                _Else2 ->
+                    cowboy_req:reply(200, [], templates:blog([
+                                                              {blog, [{blog_title, Blog#blog.title},
+                                                                      {blog_post, Blog#blog.content}]}
+                                                             ] ++ context:ctx()), Req2)
+            end
     end,
     {ok, Req, State}.
 
